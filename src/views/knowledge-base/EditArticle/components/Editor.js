7233f3a3e4613fde643a2c8b06338f3f98f09093
Ifeanyi Kelvin Ossai <ifeanyiko4u@gmail.com>
2022-12-24 18:05:08 +0100

    updated business services input fields

--- src/views/knowledge-base/EditArticle/components/Editor.js
+++ src/views/knowledge-base/EditArticle/components/Editor.js
@@ -300,6 +300,239 @@ const Editor = ({}) => {
             ></ServiceInputForm>
           )
         }
+        {/* Business Services */}
+        {
+          // book-descriptions
+          id === "book-descriptions" && (
+            <ServiceInputForm
+              data={{
+                field1: "BOOK TITLE",
+                field1Short: "Title of your book",
+                field1Place: "e.g The Coffee Roaster's Guidebook",
+                field2: "TONE",
+                field2Short:
+                  "Tone of voice your want the AI to write in. optional",
+                field2Place: "e.g excited",
+                field3: "KEYWORDS",
+                field3Short:
+                  "Keywords to be included, separated by commas, Optional.",
+                field3Place: "e.g Keyword1, Keyword2",
+              }}
+              key={id}
+              id={id}
+            ></ServiceInputForm>
+          )
+        }
+        {
+          // book-titles
+          id === "book-titles" && (
+            <ServiceInputForm
+              data={{
+                field1: "SHORT DESCRIPTION",
+                field1Short: "What your book is about",
+                field1Place: "e.g How to roast your own coffee beans",
+                field2: "BOOK TYPE",
+                field2Short: "Fiction or nonfiction",
+              }}
+              key={id}
+              id={id}
+            ></ServiceInputForm>
+          )
+        }
+        {
+          // brand-names
+          id === "brand-name" && (
+            <ServiceInputForm
+              data={{
+                field1: "SHORT DESCRIPTION",
+                field1Short: "Describe your business, product, or service",
+                field1Place: "e.g speciality coffee beans",
+                field2: "KEYWORDS",
+                field2Short:
+                  "Keywords to be included, separated by commas, Optional.",
+                field2Place: "e.g Keyword1, Keyword2",
+              }}
+              key={id}
+              id={id}
+            ></ServiceInputForm>
+          )
+        }
+        {
+          // features-benefits
+          id === "features-benefits" && (
+            <ServiceInputForm
+              data={{
+                field1: "SHORT DESCRIPTION",
+                field1Short: "What is your product about.",
+                field1Place: "e.g coffee bean roaster for home",
+                field2: "FEATURES",
+                field2Short: "State features and number them.",
+                field2Place: `e.g 1. Roaster beans under a minute
+2. Easy to Operate
+3. Easy to clean`,
+              }}
+              key={id}
+              id={id}
+            ></ServiceInputForm>
+          )
+        }
+        {
+          // marketing-ideas
+          id === "marketing-ideas" && (
+            <ServiceInputForm
+              data={{
+                field1: "SHORT DESCRIPTION",
+                field1Short: "What your product or business is about",
+                field1Place: "e.g coffee grinding machine",
+                field2: "TYPE",
+                field2Short: "Conventional or viral",
+              }}
+              key={id}
+              id={id}
+            ></ServiceInputForm>
+          )
+        }
+        {
+          // product-descriptions
+          id === "product-descriptions" && (
+            <ServiceInputForm
+              data={{
+                field1: "SHORT DESCRIPTION",
+                field1Short: "What your product or business is about",
+                field1Place:
+                  "e.g onlyAI, AI content writing tool. Easy to use interface",
+                field2: "TONE",
+                field2Short: "Tone of voice you want the AI to write. Optional",
+                field2Place: "e.g excited",
+              }}
+              key={id}
+              id={id}
+            ></ServiceInputForm>
+          )
+        }
+        {
+          // product-descriptions
+          id === "product-names" && (
+            <ServiceInputForm
+              data={{
+                field1: "SHORT DESCRIPTION",
+                field1Short: "What your product is about",
+                field1Place: "e.g home coffee bean roaster",
+                field2: "TONE",
+                field2Short: "How the name should sound",
+                field2Place: "e.g excited",
+              }}
+              key={id}
+              id={id}
+            ></ServiceInputForm>
+          )
+        }
+        {
+          // reste-des
+          id === "reste-des" && (
+            <ServiceInputForm
+              data={{
+                field1: "SHORT DESCRIPTION",
+                field1Short:
+                  "Briefly describe the property. In short sentences.",
+                field1Place:
+                  "e.g 2BR Apartment in downtown Manhattan. 1000 sqft. Askin $2M nego. Amazing city views",
+                field2: "TONE",
+                field2Short: "How the name should sound",
+                field2Place: "e.g excited",
+              }}
+              key={id}
+              id={id}
+            ></ServiceInputForm>
+          )
+        }
+        {
+          // review-responder
+          id === "review-responder" && (
+            <ServiceInputForm
+              data={{
+                field1: "CUSTOMER REVIEW",
+                field1Short: "What the customer wrote",
+                field1Place:
+                  "e.g I've been using OnlyAI for a while now and I'm really happy with it. It's helped me free up a lot of time that I would have otherwise spent on writing content.",
+                field2: "TONE",
+                field2Short:
+                  "Tone of voice you want the AI to write in. Optional",
+                field2Place: "e.g excited",
+              }}
+              key={id}
+              id={id}
+            ></ServiceInputForm>
+          )
+        }
+        {
+          // startup-pitch
+          id === "startup-pitch" && (
+            <ServiceInputForm
+              data={{
+                field1: "SHORT DESCRIPTION",
+                field1Short:
+                  "What your startup is about. Use bullet points or short sentences",
+                field1Place: `e.g - AI content writing tool`,
+                field2: "TONE",
+                field2Short:
+                  "Tone of voice you want the AI to write in. Optional",
+                field2Place: "e.g excited",
+              }}
+              key={id}
+              id={id}
+            ></ServiceInputForm>
+          )
+        }
+        {
+          // startup-ideas
+          id === "startup-ideas" && (
+            <ServiceInputForm
+              data={{
+                field1: "YOUR EXPERTISE",
+                field1Short: "What you're really good at",
+                field1Place: `e.g teaching`,
+                field2: "YOUR PASSION",
+                field2Short: "What you're really passionate about",
+                field2Place: "e.g coffee",
+              }}
+              key={id}
+              id={id}
+            ></ServiceInputForm>
+          )
+        }
+        {
+          // startup-slogans
+          id === "startup-slogans" && (
+            <ServiceInputForm
+              data={{
+                field1: "SHORT DESCRIPTION",
+                field1Short: "What your product is about.",
+                field1Place: `e.g reusable water bottle`,
+              }}
+              key={id}
+              id={id}
+            ></ServiceInputForm>
+          )
+        }
+        {
+          // startup-value
+          id === "startup-value" && (
+            <ServiceInputForm
+              data={{
+                field1: "SHORT DESCRIPTION",
+                field1Short: "What your product is about",
+                field1Place: `e.g gaming chair`,
+                field2: "TONE",
+                field2Short:
+                  "Tone of voice you want the AI to write in. Optional",
+                field2Place: "e.g excited",
+              }}
+              key={id}
+              id={id}
+            ></ServiceInputForm>
+          )
+        }
         {
           // Vision-Mission
           id === "vision-mission" && (
--- src/views/knowledge-base/EditArticle/components/ServiceInputForm.js
+++ src/views/knowledge-base/EditArticle/components/ServiceInputForm.js
@@ -1,185 +1,338 @@
-import React, { useState, useEffect, useRef } from 'react'
-import { Input, Button, Spinner, Select } from 'components/ui'
-import { writeInOpenAI } from './openAIMachine';
-import { useParams } from 'react-router-dom';
-import { useDispatch } from 'react-redux';
-import { setAiResultText } from '../store/stateSlice';
-
+import React, { useState, useEffect, useRef } from "react";
+import { Input, Button, Spinner, Select } from "components/ui";
+import { writeInOpenAI } from "./openAIMachine";
+import { useParams } from "react-router-dom";
+import { useDispatch } from "react-redux";
+import { setAiResultText } from "../store/stateSlice";
 
 const ServiceInputForm = ({ data, id }) => {
+  const [vidData, setVidData] = useState({});
 
+  const [isLoading, setIsLoading] = useState(false);
+  const inputRef = useRef(null);
 
+  const channelOptions = [
+    { value: "youtube", label: "youtube" },
+    { value: "facebook", label: "facebook" },
+    { value: "instagram", label: "instagram" },
+    { value: "tiktok", label: "tiktok" },
+  ];
+  const genreOptions = [
+    { value: "fantasy", label: "fantasy" },
+    { value: "horror", label: "horror" },
+    { value: "romance", label: "romance" },
+    { value: "seicence fiction", label: "seicence fiction" },
+  ];
+  const bookTypeOptions = [
+    { value: "fiction", label: "fiction" },
+    { value: "non-fiction", label: "non-fiction" },
+  ];
+  const marketingIdeasOption = [
+    { value: "conventional", label: "conventional" },
+    { value: "viral", label: "viral" },
+  ];
 
-    const [vidData, setVidData] = useState({});
+  const onStatusFilterChange = (selected) => {
+    setVidData({ ...vidData, channel: selected.value });
+  };
+  const dispatch = useDispatch();
+  const handleVidIdeaSubmit = async (e) => {
+    e.preventDefault();
 
-
-    const [isLoading, setIsLoading] = useState(false);
-    const inputRef = useRef(null);
-
-    const channelOptions = [
-        { value: 'youtube', label: 'youtube' },
-        { value: 'facebook', label: 'facebook' },
-        { value: 'instagram', label: 'instagram' },
-        { value: 'tiktok', label: 'tiktok' }
-
-    ]
-    const genreOptions = [
-        { value: 'fantasy', label: 'fantasy' },
-        { value: 'horror', label: 'horror' },
-        { value: 'romance', label: 'romance' },
-        { value: 'seicence fiction', label: 'seicence fiction' }
-
-    ]
-
-    const onStatusFilterChange = (selected) => {
-        setVidData({ ...vidData, channel: selected.value })
+    let finalVidStr;
+    // video ideas
+    if (id === "30") {
+      finalVidStr =
+        "tell me 5 viral video ideas of " +
+        vidData.shortDes +
+        " on " +
+        vidData.channel +
+        " in a list";
     }
-    const dispatch = useDispatch();
-    const handleVidIdeaSubmit = async (e) => {
-        e.preventDefault();
+    // social media posts
+    if (id === "31") {
+      finalVidStr = "write few social media posts about " + vidData.shortDes;
+    }
+    // image captions
+    if (id === "27") {
+      finalVidStr =
+        "write few " + vidData.tone + " image captions for " + vidData.shortDes;
+    }
+    // personal opinion
+    if (id === "28") {
+      finalVidStr = `write down your 5 ${
+        vidData?.tone ? vidData?.tone : ""
+      } opinion about ${vidData.shortDes}`;
+    }
+    // video description
+    if (id === "29") {
+      finalVidStr = `write down 5 ${
+        vidData?.tone ? vidData?.tone : ""
+      } video description about ${vidData.shortDes}`;
+    }
+    // video titles
+    if (id === "32") {
+      finalVidStr = `write down 5 video titles about ${vidData.shortDes}`;
+    }
+    // cover latter
+    if (id === "cover-latters") {
+      finalVidStr = `Write a cover letter to apply as a ${
+        vidData.shortDes
+      } i have ${vidData.skillExp} in ${
+        vidData?.tone ? vidData?.tone : "positive"
+      } tone `;
+    }
+    // fictional-story
+    if (id === "fictional-story") {
+      finalVidStr = `Write a ${vidData.channel} fictional story for ${vidData.shortDes}`;
+      console.log(finalVidStr);
+    }
+    // food-recipe
+    if (id === "food-recipe") {
+      finalVidStr = `Write a food recipe for ${vidData.shortDes}`;
+    }
+    if (id === "quora-answers") {
+      finalVidStr = `Write the ${
+        vidData?.tone ? vidData?.tone : ""
+      } answer for this question ${vidData.shortDes} from quora`;
+    }
+    if (id === "song-lyrics") {
+      finalVidStr = `Write a ${
+        vidData?.tone ? vidData?.tone : ""
+      } song lyrics for ${vidData.shortDes}`;
+    }
+    if (id === "job-description") {
+      finalVidStr = `Write a ${
+        vidData?.tone ? vidData?.tone : ""
+      } job description for ${vidData.shortDes}`;
+    }
+    if (id === "poems") {
+      finalVidStr = `Write a ${vidData?.tone ? vidData?.tone : ""} poems for ${
+        vidData.shortDes
+      }`;
+    }
+    if (id === "pd-reviews") {
+      finalVidStr = `Write a ${
+        vidData?.tone ? vidData?.tone : ""
+      } product review about ${vidData.shortDes}`;
+    }
+    if (id === "linkedin-bio") {
+      finalVidStr = `Write a ${
+        vidData?.tone ? vidData?.tone : ""
+      } linkedin bio for ${vidData.shortDes}`;
+    }
 
-        let finalVidStr;
-        // video ideas
-        if (id === "30") {
-            finalVidStr = "tell me 5 viral video ideas of " + vidData.shortDes + " on " + vidData.channel + " in a list";
-        }
-        // social media posts
-        if (id === "31") {
-            finalVidStr = "write few social media posts about " + vidData.shortDes;
-        }
-        // image captions
-        if (id === "27") {
-            finalVidStr = "write few " + vidData.tone + " image captions for " + vidData.shortDes;
-        }
-        // personal opinion 
-        if (id === "28") {
-            finalVidStr = `write down your 5 ${vidData?.tone ? vidData?.tone : ""} opinion about ${vidData.shortDes}`;
-        }
-        // video description
-        if (id === "29") {
-            finalVidStr = `write down 5 ${vidData?.tone ? vidData?.tone : ""} video description about ${vidData.shortDes}`;
-        }
-        // video titles
-        if (id === "32") {
-            finalVidStr = `write down 5 video titles about ${vidData.shortDes}`;
-        }
-        // cover latter
-        if (id === "cover-latters") {
-            finalVidStr = `Write a cover letter to apply as a ${vidData.shortDes} i have ${vidData.skillExp} in ${vidData?.tone ? vidData?.tone : "positive"} tone `;
+    setIsLoading(true);
+    if (finalVidStr) {
+      const result = await writeInOpenAI({ prompt: finalVidStr });
+      if (result.length) {
+        dispatch(setAiResultText(result));
+      }
+    }
+    setIsLoading(false);
+  };
 
-        }
-        // fictional-story
-        if (id === "fictional-story") {
-            finalVidStr = `Write a ${vidData.channel} fictional story for ${vidData.shortDes}`;
-            console.log(finalVidStr);
+  return (
+    <form
+      onSubmit={handleVidIdeaSubmit}
+      className="bg-neutral-100	rounded-md py-8 px-4 mb-2"
+    >
+      <div>
+        <label className="font-bold text-base text-stone-600">
+          {data.field1}
+        </label>
+        <small className="text-gray-500 block mb-2">{data.field1Short}</small>
 
-        }
-        // food-recipe
-        if (id === "food-recipe") {
-            finalVidStr = `Write a food recipe for ${vidData.shortDes}`;
-
-        }
-        if (id === "quora-answers") {
-            finalVidStr = `Write the ${vidData?.tone ? vidData?.tone : ""} answer for this question ${vidData.shortDes} from quora`;
-
-        }
-        if (id === "song-lyrics") {
-            finalVidStr = `Write a ${vidData?.tone ? vidData?.tone : ""} song lyrics for ${vidData.shortDes}`;
-
-        }
-        if (id === "job-description") {
-            finalVidStr = `Write a ${vidData?.tone ? vidData?.tone : ""} job description for ${vidData.shortDes}`;
-
-        }
-        if (id === "poems") {
-            finalVidStr = `Write a ${vidData?.tone ? vidData?.tone : ""} poems for ${vidData.shortDes}`;
-
-        }
-        if (id === "pd-reviews") {
-            finalVidStr = `Write a ${vidData?.tone ? vidData?.tone : ""} product review about ${vidData.shortDes}`;
-
-        }
-        if (id === "linkedin-bio") {
-            finalVidStr = `Write a ${vidData?.tone ? vidData?.tone : ""} linkedin bio for ${vidData.shortDes}`;
-
-        }
-
-        setIsLoading(true);
-        if (finalVidStr) {
-            const result = await writeInOpenAI({ prompt: finalVidStr });
-            if (result.length) {
-                dispatch(setAiResultText(result))
+        {id === "greetings" ||
+        id === "song-lyrics" ||
+        id === "job-description" ||
+        id === "linkedin-bio" ? (
+          <textarea
+            onChange={(e) =>
+              setVidData({ ...vidData, shortDes: e.target.value })
             }
+            type="text"
+            name="shortDes"
+            rows={id === "linkedin-bio" ? 4 : 2}
+            placeholder={data.field1Place}
+            class="w-full block px-3 py-2 border-b-2 border-gray-400 outline-none  focus:border-gray-600 bg-transparent"
+          />
+        ) : (
+          <input
+            onChange={(e) =>
+              setVidData({ ...vidData, shortDes: e.target.value })
+            }
+            type="text"
+            name="shortDes"
+            placeholder={data.field1Place}
+            class="w-full block px-3 py-2 border-b-2 border-gray-400 outline-none  focus:border-gray-600 bg-transparent"
+          />
+        )}
+      </div>
 
-        }
-        setIsLoading(false);
-    }
+      {id === "30" ||
+      id === "fictional-story" ||
+      id === "book-titles" ||
+      id === "marketing-ideas" ? (
+        <div className="mt-8">
+          <label className="font-bold text-base text-stone-600">
+            {data.field2}
+          </label>
+          <small className="text-gray-500 block mb-2">{data.field2Short}</small>
 
+          <Select
+            onChange={onStatusFilterChange}
+            className="w-full block  outline-none "
+            placeholder="Choose an option"
+            options={
+              id === "30"
+                ? channelOptions
+                : id === "fictional-story"
+                ? genreOptions
+                : id === "book-titles"
+                ? bookTypeOptions
+                : id === "marketing-ideas"
+                ? marketingIdeasOption
+                : ""
+            }
+          ></Select>
+        </div>
+      ) : (
+        ""
+      )}
 
-    return (
-        <form onSubmit={handleVidIdeaSubmit} className='bg-neutral-100	rounded-md py-8 px-4 mb-2'>
-            <div>
-                <label className='font-bold text-base text-stone-600'>{data.field1}</label>
-                <small className='text-gray-500 block mb-2'>{data.field1Short}</small>
+      {id === "cover-latters" && (
+        <div className="mt-8">
+          <label className="font-bold text-base text-stone-600">
+            {data.field3}
+          </label>
+          <small className="text-gray-500 block mb-2">{data.field3Short}</small>
 
-                {id === "greetings" || id === "song-lyrics" || id === "job-description" || id === "linkedin-bio" ?
-                    <textarea
-                        onChange={(e) => setVidData({ ...vidData, shortDes: e.target.value })}
-                        type="text"
-                        name="shortDes"
-                        rows={id === "linkedin-bio" ? 4 : 2}
-                        placeholder={data.field1Place}
-                        class="w-full block px-3 py-2 border-b-2 border-gray-400 outline-none  focus:border-gray-600 bg-transparent"
-                    /> : <input
-                        onChange={(e) => setVidData({ ...vidData, shortDes: e.target.value })}
-                        type="text"
-                        name="shortDes"
-                        placeholder={data.field1Place}
-                        class="w-full block px-3 py-2 border-b-2 border-gray-400 outline-none  focus:border-gray-600 bg-transparent"
-                    />}
-            </div>
+          <textarea
+            onChange={(e) =>
+              setVidData({ ...vidData, skillExp: e.target.value })
+            }
+            type="text"
+            name="skillExp"
+            placeholder={data.field3Place}
+            class="w-full block px-3 py-2 border-b-2 border-gray-400 outline-none  focus:border-gray-600 bg-transparent"
+          />
+        </div>
+      )}
 
-            {id === "30" || id === "fictional-story" ? <div className='mt-8'>
-                <label className='font-bold text-base text-stone-600'>{data.field2}</label>
-                <small className='text-gray-500 block mb-2'>{data.field2Short}</small>
+      {id === "book-descriptions" && (
+        <div className="mt-8">
+          <label className="font-bold text-base text-stone-600">
+            {data.field3}
+          </label>
+          <small className="text-gray-500 block mb-2">{data.field3Short}</small>
 
-                <Select onChange={onStatusFilterChange} className="w-full block  outline-none " placeholder="Choose an option" options={id === "30" ? channelOptions : id === "fictional-story" ? genreOptions : ""}></Select>
-            </div> : ""}
+          <textarea
+            onChange={(e) =>
+              setVidData({ ...vidData, skillExp: e.target.value })
+            }
+            type="text"
+            name="keywords"
+            placeholder={data.field3Place}
+            class="w-full block px-3 py-2 border-b-2 border-gray-400 outline-none  focus:border-gray-600 bg-transparent"
+          />
+        </div>
+      )}
 
-            {id === "cover-latters" && <div className='mt-8'>
-                <label className='font-bold text-base text-stone-600'>{data.field3}</label>
-                <small className='text-gray-500 block mb-2'>{data.field3Short}</small>
+      {id === "features-benefits" && (
+        <div className="mt-8">
+          <label className="font-bold text-base text-stone-600">
+            {data.field2}
+          </label>
+          <small className="text-gray-500 block mb-2">{data.field2Short}</small>
 
-                <textarea
-                    onChange={(e) => setVidData({ ...vidData, skillExp: e.target.value })}
-                    type="text"
-                    name="skillExp"
-                    placeholder={data.field3Place}
-                    class="w-full block px-3 py-2 border-b-2 border-gray-400 outline-none  focus:border-gray-600 bg-transparent"
-                />
-            </div>}
+          <textarea
+            onChange={(e) =>
+              setVidData({ ...vidData, skillExp: e.target.value })
+            }
+            type="text"
+            name="skillExp"
+            placeholder={data.field2Place}
+            class="w-full block px-3 py-2 border-b-2 border-gray-400 outline-none  focus:border-gray-600 bg-transparent"
+          />
+        </div>
+      )}
+      {id === "startup-ideas" && (
+        <div className="mt-8">
+          <label className="font-bold text-base text-stone-600">
+            {data.field2}
+          </label>
+          <small className="text-gray-500 block mb-2">{data.field2Short}</small>
 
-            {id === "28" || id === "29" || id === "27" || id === "cover-latters" || id === "quora-answers" || id === "song-lyrics" || id === "job-description" || id === "poems" || id === "pd-reviews" || id === "linkedin-bio" ? <div className='mt-8'>
-                <label className='font-bold text-base text-stone-600'>{data.field2}</label>
-                <small className='text-gray-500 block mb-2'>{data.field2Short}</small>
-                <input
-                    onChange={(e) => setVidData({ ...vidData, tone: e.target.value })}
-                    type="text"
-                    name="tone"
-                    placeholder={data.field2Place}
-                    class="w-60 block px-3 py-2 border-b-2 border-gray-400 outline-none  focus:border-gray-600 bg-transparent"
-                />
-            </div> : ""}
-            {isLoading ?
-                <Button className=" mt-5 py-2 tracking-wide" size="sm" block variant="solid" >
-                    <Spinner className="mx-auto block" color="white" />
-                </Button>
-                :
-                <Button type="submit" className=" mt-5 py-2 tracking-wide" size="sm" block variant="solid" >WRITE FOR ME</Button>}
+          <textarea
+            onChange={(e) =>
+              setVidData({ ...vidData, skillExp: e.target.value })
+            }
+            type="text"
+            name="skillExp"
+            placeholder={data.field2Place}
+            class="w-full block px-3 py-2 border-b-2 border-gray-400 outline-none  focus:border-gray-600 bg-transparent"
+          />
+        </div>
+      )}
 
-        </form>
-    )
-}
-
+      {id === "28" ||
+      id === "29" ||
+      id === "27" ||
+      id === "cover-latters" ||
+      id === "quora-answers" ||
+      id === "song-lyrics" ||
+      id === "job-description" ||
+      id === "poems" ||
+      id === "pd-reviews" ||
+      id === "linkedin-bio" ||
+      id === "book-descriptions" ||
+      id === "brand-name" ||
+      id === "product-descriptions" ||
+      id === "product-names" ||
+      id === "reste-des" ||
+      id === "review-responder" ||
+      id === "startup-pitch" ||
+      id === "startup-value" ||
+      id === "vision-mission" ? (
+        <div className="mt-8">
+          <label className="font-bold text-base text-stone-600">
+            {data.field2}
+          </label>
+          <small className="text-gray-500 block mb-2">{data.field2Short}</small>
+          <input
+            onChange={(e) => setVidData({ ...vidData, tone: e.target.value })}
+            type="text"
+            name="tone"
+            placeholder={data.field2Place}
+            class="w-60 block px-3 py-2 border-b-2 border-gray-400 outline-none  focus:border-gray-600 bg-transparent"
+          />
+        </div>
+      ) : (
+        ""
+      )}
+      {isLoading ? (
+        <Button
+          className=" mt-5 py-2 tracking-wide"
+          size="sm"
+          block
+          variant="solid"
+        >
+          <Spinner className="mx-auto block" color="white" />
+        </Button>
+      ) : (
+        <Button
+          type="submit"
+          className=" mt-5 py-2 tracking-wide"
+          size="sm"
+          block
+          variant="solid"
+        >
+          WRITE FOR ME
+        </Button>
+      )}
+    </form>
+  );
+};
 
 export default ServiceInputForm;
